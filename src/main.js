f0a2940ed5cae238341ec46a61a3df9166bca09b
Vasiliy Aleksandrov <v.aleksandrov@adguard.com>
2020-11-21 14:24:07 +0300

    module1-task3 add modals

--- src/main.js
+++ src/main.js
@@ -1,12 +1,29 @@
-import { createSiteInfoTemplate } from "./view/site-info.js";
-import { createSiteMenuTemplate } from "./view/site-menu.js";
+import { createInfoTemplate } from "./view/info.js";
+import { createMenuTemplate } from "./view/menu.js";
+import { createFiltersTemplate } from "./view/filters.js";
+import { createSortTemplate } from "./view/events.js";
+import { createEditFormTemplate } from "./view/edit-form.js";
+import { createCreateFormTemplate } from "./view/create-form.js";
+import { createPointTemplate } from "./view/point.js";
 
 const render = (container, template, place) => {
     container.insertAdjacentHTML(place, template);
 };
+
+const POINTS_COUNT = 3;
   
 const siteTripMainElement = document.querySelector(`.trip-main`);
 const siteTripControlsElement = document.querySelector(`.trip-controls`);
+const siteTripEventsElement = document.querySelector(`.trip-events`);
+const siteTripEventsListElement = document.querySelector(`.trip-events__list`);
 
-render(siteTripMainElement, createSiteInfoTemplate(), `afterbegin`);
-render(siteTripControlsElement, createSiteMenuTemplate(), `afterbegin`);
+render(siteTripMainElement, createInfoTemplate(), `afterbegin`);
+render(siteTripControlsElement, createMenuTemplate(), `afterbegin`);
+render(siteTripControlsElement, createFiltersTemplate(), `beforeend`);
+render(siteTripEventsElement, createSortTemplate(), `afterbegin`);
+render(siteTripEventsListElement, createEditFormTemplate(), `beforeend`);
+render(siteTripEventsListElement, createCreateFormTemplate(), `beforeend`);
+
+for (let i = 0; i < POINTS_COUNT; i++) {
+  render(siteTripEventsListElement, createPointTemplate(), `beforeend`);
+}
