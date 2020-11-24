411cd96f9a04f49aa2ee5efe873a9b214d8f3a5e
Vasiliy Aleksandrov <v.aleksandrov@adguard.com>
2020-11-24 18:56:21 +0300

    module2-task1 add destination info

--- src/mock/mock.js
+++ src/mock/mock.js
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 const getRandomInteger = (a = 0, b = 1) => {
   const lower = Math.ceil(Math.min(a, b));
   const upper = Math.floor(Math.max(a, b));
@@ -5,8 +7,21 @@ const getRandomInteger = (a = 0, b = 1) => {
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
 
-const routPoints = ['Transport', 'Drive', 'Flight', 'Check-in', 'Sightseeing', 'Restaurant'];
-const cities = ['Воронеж', 'Воркута', 'Архангельск', 'Тверь', 'Томск', 'Сочи'];
+const generateDate = () => {
+  const isDate = Boolean(getRandomInteger(0, 1));
+
+  if (!isDate) {
+    return null;
+  }
+
+  const maxDaysGap = 7;
+  const daysGap = getRandomInteger(-maxDaysGap, maxDaysGap);
+
+  return dayjs().add(daysGap, `day`).toDate();
+};
+
+const routPoints = [`Transport`, `Drive`, `Flight`, `Check-in`, `Sightseeing`, `Restaurant`];
+const cities = [`Воронеж`, `Воркута`, `Архангельск`, `Тверь`, `Томск`, `Сочи`];
 
 const generateRandomItem = (items) => {
 
@@ -16,10 +31,10 @@ const generateRandomItem = (items) => {
 };
 
 const generatePointOption = () => {
-  const optionsName = ['Order uber', 'Add luggage', 'Switch to comfort', 'Rent a car', 'Add breakfast'];
+  const optionsName = [`Order uber`, `Add luggage`, `Switch to comfort`, `Rent a car`, `Add breakfast`];
   const optionPoints = [];
 
-  for (i = 0; i < getRandomInteger(0, 3); i++) {
+  for (let i = 0; i < getRandomInteger(0, 3); i++) {
     optionPoints.push({
       name: generateRandomItem(optionsName),
       cost: `+€ ${generateRandomItem(5, 100)}`,
@@ -30,10 +45,29 @@ const generatePointOption = () => {
   return optionPoints;
 };
 
+const generateDestination = () => {
+  const descriptions = [
+    `Lorem ipsum dolor sit amet, consectetur adipiscing elit.`,
+    `Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra.`,
+    `Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante.`,
+    `Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum.`,
+    `Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat.`
+  ];
+
+  return {
+    description: generateRandomItem(descriptions),
+    image: `http://picsum.photos/248/152?r=${Math.random()}`,
+  };
+};
+
 export const generateTask = () => {
-  return { 
+  const dueDate = generateDate();
+
+  return {
+    dueDate,
     routPoint: generateRandomItem(routPoints),
     destination: generateRandomItem(cities),
     options: generatePointOption(),
+    destinationInfo: generateDestination(),
   };
 };
